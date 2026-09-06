b5bcec31793dd1f7b7a0206f560e2483f17740dc
lucasew <lucas59356@gmail.com>
2026-09-06 18:39:27 -0300

    password check utilities
    Signed-off-by: lucasew <lucas59356@gmail.com>

--- go.mod
+++ go.mod
@@ -4,6 +4,7 @@ go 1.27.0
 
 require (
 	github.com/stretchr/testify v1.12.1
+	golang.org/x/crypto v0.56.0
 	golang.org/x/sync v0.22.0
 )
 
---
+++ x/auth/password.go
@@ -0,0 +1,16 @@
+package auth
+
+import "golang.org/x/crypto/bcrypt"
+
+func HashPassword(password string) (string, error) {
+	HashPasswordCost(password, bcrypt.DefaultCost)
+}
+
+func HashPasswordCost(password string, cost int) (string, error) {
+	b, err := bcrypt.GenerateFromPassword([]byte(password), cost)
+	return string(b), err
+}
+
+func CheckHashedPassword(hash, pw string) bool {
+	return bcrypt.CompareHashAndPassword([]byte(hash), []byte(pw)) == nil
+}
