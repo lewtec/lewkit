9764c822f2fa34da64bd0caffb5cb24cb883509d
lucasew <15693688+lucasew@users.noreply.github.com>
2026-09-15 16:56:11 +0000

    Updater script changes

--- x/cmd/enum_test.go
+++ x/cmd/enum_test.go
@@ -89,7 +89,7 @@ func TestUsageChoices(t *testing.T) {
 }
 
 var (
-	_ Parser         = (*EnumArg[color])(nil)
-	_ Arg[color]     = (*EnumArg[color])(nil)
-	_ ArgChooser     = EnumArg[color]{}
+	_ Parser     = (*EnumArg[color])(nil)
+	_ Arg[color] = (*EnumArg[color])(nil)
+	_ ArgChooser = EnumArg[color]{}
 )
