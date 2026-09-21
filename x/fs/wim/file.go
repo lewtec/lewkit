cda189dc6818ec38a58b8f93b0158f061dc1e09b
lucasew <lucas59356@gmail.com>
2026-09-21 16:54:29 +0000

    refactor(x/fs): use DirEntries in UDF and WIM dir files
    The paging copy was leftover after DirEntries landed. Call the helper.

--- x/fs/wim/file.go
+++ x/fs/wim/file.go
@@ -10,6 +10,8 @@ import (
 	"time"
 
 	winwim "github.com/Microsoft/go-winio/wim"
+
+	lewfs "github.com/lewtec/lewkit/x/fs"
 )
 
 func (n *dnode) open() (fs.File, error) {
@@ -78,24 +80,7 @@ func (d *dirFile) Read([]byte) (int, error) {
 func (d *dirFile) Close() error { return nil }
 
 func (d *dirFile) ReadDir(n int) ([]fs.DirEntry, error) {
-	if d.off >= len(d.infos) {
-		if n <= 0 {
-			return nil, nil
-		}
-		return nil, io.EOF
-	}
-	if n <= 0 {
-		out := d.infos[d.off:]
-		d.off = len(d.infos)
-		return out, nil
-	}
-	end := d.off + n
-	if end > len(d.infos) {
-		end = len(d.infos)
-	}
-	out := d.infos[d.off:end]
-	d.off = end
-	return out, nil
+	return lewfs.DirEntries(d.infos, &d.off, n)
 }
 
 type fileInfo struct {
